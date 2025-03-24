aef59d9135f594d12ea6e6fbcd4c010a2a20db79
Samstep <samstep2012@gmail.com>
2025-03-23 19:02:19 -0700

    code works output in output.log given 8 test cases

--- tcas_simulator.v
+++ tcas_simulator.v
@@ -9,7 +9,8 @@
 */
 
 module tcas_simulator(
-    input clk, om,                                 // Clock & Operation Mode Bit
+    input clk, 
+    input [1:0] om,                                 // Clock & Operation Mode Bit
     input wire signed [15:0] p1x, p1y, p1z,        // Position 1 Vector
     input wire signed [15:0] v1x, v1y, v1z,        // Velocity 1 Vector
     input wire signed [31:0] p2x, p2y, p2z,        // Position 2 Vector
@@ -20,7 +21,7 @@ module tcas_simulator(
 );
 
 // +==========[ STATE CONTROL ]==========+
-reg [1:0] CS, NS;
+reg [1:0] CS = 0, NS = 0;
 
 // +==========[ STATES ]==========+
 parameter STARTUP   = 2'b00,
@@ -28,18 +29,17 @@ parameter STARTUP   = 2'b00,
           OPERATING = 2'b10;
 
 // +==========[ TCAS LEVELS ]==========+
-parameter TRAFFIC_RESET     = 2'b00,
-          TRAFFIC_NONE      = 2'b01,
-          TRAFFIC_ADVISORY  = 2'b10;
+parameter TRAFFIC_NONE     = 2'b00,
+          TRAFFIC_ADVISORY = 2'b01;
 
 // +==========[ TCAS Operation Modes ]==========+
 parameter OM_RESET   = 2'b00,
           OM_TA_ONLY = 2'b01,
           OM_TA_RA   = 2'b10;
-        
+
 // +==========[ TCAS RESOLUTIONS ]==========+
-parameter RA_RESET   = 3'b000,
-          RA_NONE    = 3'b001,
+parameter RA_NONE    = 3'b000,
+          RA_TRIGGER = 3'b001,
           RA_DESCEND = 3'b010,
           RA_CLIMB   = 3'b011;
 
@@ -50,10 +50,11 @@ reg [2:0] ta, ra; // Advisory Flags
 
 always @(posedge clk or om)
 begin
-    if(om == OM_RESET)
-        CS <= STARTUP;
-    else
-        CS <= NS;
+    // $display("Current State: %b, OM: %b", CS, om);
+    if(om == OM_RESET) begin
+        CS <= STARTUP; end
+    else begin
+        CS <= NS; end
 end
 
 always @(*)
@@ -62,27 +63,28 @@ begin
         STARTUP: 
         begin 
             if(om != OM_RESET)
-                NS = STANDBY;
+                NS <= STANDBY;
             else
-                NS = STARTUP; // Stay in startup state
+                NS <= STARTUP; // Stay in startup state
         end
-        STANDBY: 
+        STANDBY:
         begin
             if(om == OM_TA_ONLY || om == OM_TA_RA)
-                NS = OPERATING;
+                NS <= OPERATING;
             else
-                NS = STANDBY;
+                NS <= STANDBY;
         end
         OPERATING:
         begin
+            $display("In Operating");
             if(om == OM_TA_RA)
-                NS = OPERATING;
+                NS <= OPERATING;
             else if(om == OM_TA_ONLY)
-                NS = OPERATING;
+                NS <= OPERATING;
             else
-                NS = STANDBY;
+                NS <= STANDBY;
         end
-        default: NS = STARTUP;
+        default: NS <= STARTUP;
     endcase
 end
 
@@ -91,8 +93,8 @@ begin
     case(CS)
         STARTUP: 
         begin
-            tcas_traffic <= TRAFFIC_RESET;
-            tcas_resolution <= RA_RESET;
+            tcas_traffic <= TRAFFIC_NONE;
+            tcas_resolution <= RA_NONE;
         end
         STANDBY:
         begin
@@ -106,14 +108,14 @@ begin
 
             if(om == OM_TA_RA)
             begin
-                ;
-                // threat_detection(p1x, p1y, p1z, p2x, p2y, p2z, v1x, v1y, v1z, v2x, v2y, v2z, <missing vars>);
-                // advise_maneuver(p1x, p1y, p1z, p2x, p2y, p2z, v1x, v1y, v1z, v2x, v2y, v2z, vmax, amax, react);
+                threat_detection (p1x, p1y, p1z, p2x, p2y, p2z, v1x, v1y, v1z, v2x, v2y, v2z, altitude, ta, ra);
+                if(ra == RA_TRIGGER)
+                    advise_maneuver (p1x, p1y, p1z, p2x, p2y, p2z, v1x, v1y, v1z, v2x, v2y, v2z, vmax, amax, react, ra);
             end
             else if(om == OM_TA_ONLY)
             begin
-                ;
-                // threat_detection(p1x, p1y, p1z, p2x, p2y, p2z, v1x, v1y, v1z, v2x, v2y, v2z, <missing vars>);
+                threat_detection (p1x, p1y, p1z, p2x, p2y, p2z, v1x, v1y, v1z, v2x, v2y, v2z, altitude, ta, ra);
+                // $display("Hello ta = %d, ra = %d", ta, ra);
             end
             else
             ;
@@ -124,18 +126,116 @@ end
 
 endmodule
 
-module advise_maneuver(
-    input wire signed [31:0] p1_x, p1_y, p1_z,
-    input wire signed [31:0] p2_x, p2_y, p2_z,
-    input wire signed [31:0] v1_x, v1_y, v1_z,
-    input wire signed [31:0] v2_x, v2_y, v2_z,
-    input wire signed [31:0] vmax, amax, react,
+task thresholds(
+    input [31:0] altitude,         // 32-bit input representing the altitude
+    output reg [31:0] tau_ta_th,   // Output tau_ta_th
+    output reg [31:0] range_ta_th, // Output range_ta_th
+    output reg [31:0] vert_ta_th,  // Output vert_ta_th
+    output reg [31:0] tau_ra_th,   // Output tau_ra_th
+    output reg [31:0] range_ra_th, // Output range_ra_th
+    output reg [31:0] vert_ra_th   // Output vert_ra_th
+);
+    integer sl;
+
+    // always@(*)
+    begin
+        if (altitude < 3280) begin
+            sl = 2;
+        end else if (altitude < 7710) begin
+            sl = 3;
+        end else if (altitude < 16400) begin
+            sl = 4;
+        end else if (altitude < 32800) begin
+            sl = 5;
+        end else if (altitude < 65600) begin
+            sl = 6;
+        end else if (altitude < 137760) begin
+            sl = 7;
+        end else begin
+            sl = 8;
+        end
+        // $display("sl = %d", sl);
+        // Select the threshold values based on 'sl'
+        case (sl)
+            2: begin
+                tau_ta_th = 400;
+                range_ta_th = 560;
+                vert_ta_th = 260;
+                tau_ra_th = 0;
+                range_ra_th = 0;
+                vert_ra_th = 0;
+            end
+            3: begin
+                tau_ta_th = 625;
+                range_ta_th = 615;
+                vert_ta_th = 260;
+                tau_ra_th = 225;
+                range_ra_th = 375;
+                vert_ra_th = 185;
+            end
+            4: begin
+                tau_ta_th = 900;
+                range_ta_th = 890;
+                vert_ta_th = 260;
+                tau_ra_th = 400;
+                range_ra_th = 650;
+                vert_ra_th = 185;
+            end
+            5: begin
+                tau_ta_th = 1600;
+                range_ta_th = 1390;
+                vert_ta_th = 260;
+                tau_ra_th = 625;
+                range_ra_th = 1020;
+                vert_ra_th = 185;
+            end
+            6: begin
+                tau_ta_th = 2025;
+                range_ta_th = 1855;
+                vert_ta_th = 260;
+                tau_ra_th = 900;
+                range_ra_th = 1485;
+                vert_ra_th = 185;
+            end
+            7: begin
+                tau_ta_th = 2304;
+                range_ta_th = 2410;
+                vert_ta_th = 260;
+                tau_ra_th = 1225;
+                range_ra_th = 2040;
+                vert_ra_th = 215;
+            end
+            8: begin
+                tau_ta_th = 2304;
+                range_ta_th = 2410;
+                vert_ta_th = 365;
+                tau_ra_th = 1225;
+                range_ra_th = 2040;
+                vert_ra_th = 250;
+            end
+            default: begin
+                tau_ta_th = 0;
+                range_ta_th = 0;
+                vert_ta_th = 0;
+                tau_ra_th = 0;
+                range_ra_th = 0;
+                vert_ra_th = 0;
+            end
+        endcase
+    end
+endtask
+
+task advise_maneuver(
+    input signed [31:0] p1_x, p1_y, p1_z,
+    input signed [31:0] p2_x, p2_y, p2_z,
+    input signed [31:0] v1_x, v1_y, v1_z,
+    input signed [31:0] v2_x, v2_y, v2_z,
+    input signed [31:0] vmax, amax, react,
     output reg [2:0] maneuver
 );
-
 // +==========[ TCAS RESOLUTIONS ]==========+
-parameter RA_RESET   = 3'b000,
-          RA_NONE    = 3'b001,
+parameter RA_NONE    = 3'b000,
+          RA_TRIGGER = 3'b001,
           RA_DESCEND = 3'b010,
           RA_CLIMB   = 3'b011;
           
@@ -145,88 +245,119 @@ parameter RA_RESET   = 3'b000,
     integer upDifference, downDifference;
     integer tca;
 
-    always @(*) begin
+    // always @(*) begin
         // Calculate relative vectors
-        pr_x = p1_x - p2_x;
-        pr_y = p1_y - p2_y;
-        pr_z = p1_z - p2_z;
+    pr_x = p1_x - p2_x;
+    pr_y = p1_y - p2_y;
+    pr_z = p1_z - p2_z;
 
-        vr_x = v1_x - v2_x;
-        vr_y = v1_y - v2_y;
-        vr_z = v1_z - v2_z;
+    vr_x = v1_x - v2_x;
+    vr_y = v1_y - v2_y;
+    vr_z = v1_z - v2_z;
 
-        // Calculate time of closest approach
-        tca = -(pr_x * vr_x + pr_y * vr_y + pr_z * vr_z) / (vr_x * vr_x + vr_y * vr_y + vr_z * vr_z);
+    // Calculate time of closest approach
+    tca = -(pr_x * vr_x + pr_y * vr_y + pr_z * vr_z) / (vr_x * vr_x + vr_y * vr_y + vr_z * vr_z);
 
-        // Calculate maximum vertical deviation before TCA
-        reusedValue = (v1_z * v1_z - vmax * vmax) / (2 * amax);
-        upDeviation = vmax * (tca - react - (-v1_z + vmax) / amax) + (-reusedValue + react * v1_z);
-        downDeviation = -vmax * (tca - react - (v1_z + vmax) / amax) + (reusedValue + react * v1_z);
+    // Calculate maximum vertical deviation before TCA
+    reusedValue = ((v1_z * v1_z) - (vmax * vmax)) / (2 * amax);
+    downDeviation = -vmax*(tca - react - (v1_z + vmax)/amax) + (reusedValue + react*v1_z);
+    upDeviation = vmax*(tca - react - (-v1_z + vmax)/amax) + (-reusedValue + react*v1_z);
 
-        // Calculate the final vertical deviations
-        upDifference = (p1_z + upDeviation - p2_z + tca * v2_z);
-        downDifference = (p1_z + downDeviation - p2_z + tca * v2_z);
+    // Calculate the final vertical deviations
+    upDifference = (p1_z + upDeviation - p2_z + tca * v2_z);
+    downDifference = (p1_z + downDeviation - p2_z + tca * v2_z);
 
-        // abs equivalent
-        upDifference = upDifference > 0 ? upDifference : upDifference*-1;
-        downDifference = downDifference > 0 ? downDifference : downDifference*-1;
+    // abs equivalent
+    upDifference = upDifference > 0 ? upDifference : upDifference*-1;
+    downDifference = downDifference > 0 ? downDifference : downDifference*-1;
 
-        // Determine the maneuver (climb or descend)
-        if (upDifference > downDifference) begin
-            maneuver = RA_CLIMB;  // Climb
-        end else begin
-            maneuver = RA_DESCEND;  // Descend
-        end
-    end
-endmodule
+    // Determine the maneuver (climb or descend)
+    if (upDifference > downDifference) begin
+        maneuver = RA_CLIMB;  // Climb
+    end else
+        maneuver = RA_DESCEND;  // Descend
 
-module threat_detection(
-    input wire signed [31:0] p1_x, p1_y, p1_z, p2_x, p2_y, p2_z, v1_x, v1_y, v1_z, v2_x, v2_y, v2_z,
-    input wire signed [31:0] taurange_ta_th, tauvert_ta_th, range_ta_th, vert_ta_th,
-    input wire signed [31:0] taurange_ra_th, tauvert_ra_th, range_ra_th, vert_ra_th,
+endtask
+
+task threat_detection(
+    input signed [31:0] p1_x, p1_y, p1_z, p2_x, p2_y, p2_z, v1_x, v1_y, v1_z, v2_x, v2_y, v2_z,
+    input signed [31:0] altitude,
     output reg [2:0] ta,  // Threat Advisory (TA)
     output reg [2:0] ra   // Resolution Advisory (RA)
 );
-    integer prmagsq, vrproj, taurange, tauvert, pr_x, pr_y, pr_z, vr_x, vr_y, vr_z;
+    integer prmagsq, vrmagsq, vrproj, taurange, tauvert, pr_x, pr_y, pr_z, vr_x, vr_y, vr_z;
+    integer tau_ta_th, range_ta_th, vert_ta_th, tau_ra_th, range_ra_th, vert_ra_th;
 
-    always @(*) begin
+    // +==========[ TCAS LEVELS ]==========+
+    parameter TRAFFIC_NONE = 2'b00,
+          TRAFFIC_ADVISORY = 2'b01;
+
+// +==========[ TCAS RESOLUTIONS ]==========+
+    parameter RA_NONE = 3'b000,
+          RA_TRIGGER  = 3'b001,
+          RA_DESCEND  = 3'b010,
+          RA_CLIMB    = 3'b011;
+
+    // always @(*)
+    begin
         // Calculate relative vectors
         pr_x = p1_x - p2_x;
         pr_y = p1_y - p2_y;
         pr_z = p1_z - p2_z;
 
+        // $display("pr_x = %d, pr_y = %d, pr_z = %d", pr_x, pr_y, pr_z);
+
         vr_x = v1_x - v2_x;
         vr_y = v1_y - v2_y;
         vr_z = v1_z - v2_z;
 
+        // Calculate thresholds based on altitude (p1_z)
+        thresholds (altitude, tau_ta_th, range_ta_th, vert_ta_th, tau_ra_th, range_ra_th, vert_ra_th);
+
         // Calculate magnitude squared of pr
         prmagsq = pr_x * pr_x + pr_y * pr_y + pr_z * pr_z;
+        // $display("prmagsq = %d", prmagsq);
+
+        // Calculate magnitude squared of vr
+        vrmagsq = vr_x * vr_x + vr_y * vr_y + vr_z * vr_z;
 
         // Calculate the projection of pr on vr
         vrproj = pr_x * vr_x + pr_y * vr_y + pr_z * vr_z;
+        // $display("vrproj = %d", vrproj);
 
-        // Calculate time to closest range approach 
-        taurange = prmagsq / vrproj;
+        // Calculate time to closest range approach
+        if(vrproj == 0)
+            taurange = 500;
+        else
+            taurange = prmagsq / vrmagsq;
 
-        // Calculate time to vertical range approach
-        tauvert = pr_z / vr_z;
+        // $display("taurange = %d", taurange);
+
+        if(vr_z == 0)
+            tauvert = 500;
+        else
+            tauvert = (pr_z * pr_z) / (vr_z * vr_z);
+
+        // $display("tauvert = %d", tauvert);
 
         // Default to no threat
-        ta = 0;
-        ra = 0;
+        ta = TRAFFIC_NONE;
+        ra = RA_NONE;
 
         // Check if the conditions for TA (Threat Advisory) are met
-        if (taurange < taurange_ta_th && tauvert < tauvert_ta_th) begin
-            if (prmagsq < range_ta_th && pr_z < vert_ta_th) begin
-                ta = 1;
-            end
+        if ((taurange < tau_ta_th || prmagsq < range_ta_th * range_ta_th) && (tauvert < tau_ta_th || pr_z < vert_ta_th))
+        begin
+            // $display("CONDITION PASSED FOR TA WOOO");
+            ta = TRAFFIC_ADVISORY;
         end
+        else
+            ;
+            // $display("CONDITION FAILED, %b", taurange < tau_ra_th);
 
         // Check if the conditions for RA (Resolution Advisory) are met
-        if (taurange < taurange_ra_th && tauvert < tauvert_ra_th) begin
-            if (prmagsq < range_ra_th && pr_z < vert_ra_th) begin
-                ra = 1;
-            end
-        end
+        if ((taurange < tau_ra_th || prmagsq < range_ra_th * range_ra_th) && (tauvert < tau_ra_th || pr_z < vert_ra_th))
+            ra = RA_TRIGGER;
+        else
+            ;
     end
-endmodule
+endtask
--- tcas_simulator_tb.v
+++ tcas_simulator_tb.v
@@ -1,23 +1,129 @@
 module tcas_simulator_tb();
 
-reg clk, fly;
-reg [15:0] px, py, pz, vx, vy, vz;
+reg clk;
+reg [1:0] om;
+reg signed [15:0] px_1, py_1, pz_1, vx_1, vy_1, vz_1;
+reg signed [15:0] px_2, py_2, pz_2, vx_2, vy_2, vz_2;
+reg [31:0] altitude;
 
-wire [2:0] tcas_resolution;
-wire [2:0] tcas_level;
+wire [2:0] resolution_advisory;
+wire [2:0] traffic_advisory;
+
+tcas_simulator dut (clk, om, px_1, py_1, pz_1, vx_1, vy_1, vz_1, px_2, py_2, pz_2, vx_2, vy_2, vz_2, altitude, resolution_advisory, traffic_advisory);
+
+initial begin
+    clk = 0;
+end
 
 always begin
-    #5 clk = ~clk;
+    #10 clk = ~clk;
 end
 
 initial begin
-    $display("+=========[Test Case 1]=========+");
+    $display("+=========[RESET]=========+");
+    om = 2'b00;
+    // Airplane 1
+    px_1 = 0; py_1 = 0; pz_1 = 0;
+    vx_1 = 0; vy_1 = 0; vz_1 = 0;
+    altitude = 0;
 
+    // Airplane 2
+    px_2 = 0; py_2 = 0; pz_2 = 0;
+    vx_2 = 0; vy_2 = 0; vz_2 = 0;
 
+    #100 $display("+=========[TEST 1]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -6000; pz_1 = 15000;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
 
+    // Airplane 2
+    px_2 = -6000; py_2 = 0; pz_2 = 15000;
+    vx_2 = 150; vy_2 = 0; vz_2 = 0;
 
+    om = 2'b10; // Set the operation mode
+    $display("No change so monitor won't print.");
 
-    #200 $finish
+    #100 $display("+=========[TEST 2]=========+");
+    // Airplane 1
+    
+    px_1 = 0; py_1 = -4000; pz_1 = 15000;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = -4000; py_2 = 0; pz_2 = 15000;
+    vx_2 = 150; vy_2 = 0; vz_2 = 0;
+
+    #100 $display("+=========[TEST 3]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -2500; pz_1 = 15000;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = -2500; py_2 = 0; pz_2 = 15000;
+    vx_2 = 150; vy_2 = 0; vz_2 = 0;
+
+    #100 $display("+=========[TEST 4]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -2500; pz_1 = 15100;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = -2500; py_2 = 0; pz_2 = 15000;
+    vx_2 = 150; vy_2 = 0; vz_2 = 0;
+
+    #100 $display("+=========[TEST 5]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -2500; pz_1 = 14900;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = -2500; py_2 = 0; pz_2 = 15000;
+    vx_2 = 150; vy_2 = 0; vz_2 = 0;
+
+    #100 $display("+=========[TEST 6]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -6000; pz_1 = 15000;
+    vx_1 = 0; vy_1 = 400; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = -6000; py_2 = 0; pz_2 = 15000;
+    vx_2 = 400; vy_2 = 0; vz_2 = 0;
+
+    $display("No change so monitor won't print.");
+
+    #100 $display("+=========[TEST 7]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -8000; pz_1 = 15000;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = -600; py_2 = -8000; pz_2 = 15000;
+    vx_2 = 15; vy_2 = 150; vz_2 = 0;
+
+    $display("No change so monitor won't print.");
+
+    #100 $display("+=========[TEST 8]=========+");
+    // Airplane 1
+    px_1 = 0; py_1 = -8000; pz_1 = 15000;
+    vx_1 = 0; vy_1 = 150; vz_1 = 0;
+    altitude = pz_1;
+
+    // Airplane 2
+    px_2 = 0; py_2 = -8000; pz_2 = 14900;
+    vx_2 = 0; vy_2 = 150; vz_2 = 2;
+
+    #200 $finish;
+end
+
+initial begin
+    $monitor("traffic_advisory (TA) = %d, resolution_advisory (RA) = %d @ t = ", traffic_advisory, resolution_advisory, $time);
 end
 
 endmodule
